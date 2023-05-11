3b88ab45210fa75f12523ab5ac242a160a3629b5
Desiberio <39953109+Desiberio@users.noreply.github.com>
2023-05-11 21:10:47 +0300

    Fixed query
    Найдена и исправлена ошибка благодаря обратной связи.

--- ProductsAndCategoriesQuery.sql
+++ ProductsAndCategoriesQuery.sql
@@ -1,4 +1,4 @@
 select p.Name, c.Name
 from Products p
-inner join Products_Categories pc on p.ProductId = pc.ProductId
+left join Products_Categories pc on p.ProductId = pc.ProductId /*Left Join. По какой-то причине в тестовой базе данных сработало и с Inner Join'ом, но, попробовав на другом примере, Left Join оказался правильным выбором.*/
 left join Categories c on c.CategoryId = pc.CategoryId;
